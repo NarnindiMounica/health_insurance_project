b1df08570d17bd01e5dd73ea577b843700fa359c
NarnindiMounica <mounica.narnindi12@gmail.com>
2023-10-02 20:12:56 +0530

    Hyperparameters tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -39,7 +39,45 @@ class ModelTrainer:
                 "XGBoost": XGBRegressor()
             }
 
-            model_report:dict = evaluate_models(x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test, models=models)
+            parameters = {
+                "Random Forest": {
+                    #  'criterion':[“squared_error”, “absolute_error”, “friedman_mse”, “poisson”],
+                     # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Decision Tree": {
+                    'criterion': [ 'squared_error', 'absolute_error', 'friedman_mse', 'poisson' ],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Linear Regression": {},
+                "K Neighbors": {"n_neighbors": [5,7,9,11,13],
+                                "metric": ['minkowski']
+                },
+                "Ada Boost": {
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting": {
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Cat Boosting": {
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "XGBoost": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                } }
+
+            model_report:dict = evaluate_models(x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test, models=models, parameters= parameters)
 
             logging.info('Getting best model from evaluated models')
             best_model_name = max(zip(model_report.items()))[0][0]
@@ -60,7 +98,7 @@ class ModelTrainer:
             best_pred = best_model.predict(x_test)
             best_r2_score = r2_score(y_test, best_pred)
 
-            return best_r2_score
+            return best_r2_score, best_model_name
 
         except Exception as e:
             raise CustomException(e,sys) 
--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import dill
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from src.exceptions import CustomException
 
 def save_object(file_path, object):
@@ -17,12 +18,19 @@ def save_object(file_path, object):
         raise CustomException(e, sys)
     
 
-def evaluate_models(x_train, y_train, x_test, y_test, models):
+def evaluate_models(x_train, y_train, x_test, y_test, models, parameters):
     try:
         report = {}
         for i in range(len(models)):
             model = list(models.values())[i]
+            param = list(parameters.keys())[i]
+
+            grid_search = GridSearchCV(model, param_grid = parameters[param],cv=5)
+            grid_search.fit(x_train, y_train)
+
+            model.set_params(**grid_search.best_params_)
             model.fit(x_train, y_train)
+
             train_predict = model.predict(x_train)
             test_predict = model.predict(x_test)
             train_score = r2_score(y_train, train_predict)
